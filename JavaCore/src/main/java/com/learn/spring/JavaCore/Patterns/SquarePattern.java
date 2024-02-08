192e0060764232507199adda7bf62c6d4f2b0191
tharun <thulasitharun1@gmail.com>
2024-02-08 14:40:36 +0530

    Square pattern 5*5

---
+++ JavaCore/src/main/java/com/learn/spring/JavaCore/Patterns/SquarePattern.java
@@ -0,0 +1,19 @@
+package com.learn.spring.JavaCore.Patterns;
+
+public class SquarePattern {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Integer a = 5;
+		Integer b = 5;
+		for(int i=1;i<=5;i++)
+		{
+			for(int j=1;j<=5;j++)
+			{
+				System.out.print("* ");
+			}
+			System.out.println();
+		}
+	}
+
+}
