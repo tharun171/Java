aabfc1b9963f0586b26c3e13ddbca472096ccdff
tharun <thulasitharun1@gmail.com>
2024-02-17 16:06:15 +0530

    changed package name

---
+++ JavaCore/src/main/java/com/Java/StandardEditionTopics/SwitchExample.java
@@ -0,0 +1,28 @@
+package com.Java.StandardEdition;
+
+public class SwitchExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		char character = 'w';
+		
+		switch(character)
+		{
+		case 'a':
+			System.out.println("entered a ");
+			System.out.println("entered A");
+			//if break is not written here
+			//everything below cases other than a also be printed
+			break;
+		case 'b':
+			System.out.println("entered b");
+			System.out.println("entered B");
+			break;
+		default:
+			System.out.println("default code when switch cant find the condition");
+		}
+
+	}
+
+}
