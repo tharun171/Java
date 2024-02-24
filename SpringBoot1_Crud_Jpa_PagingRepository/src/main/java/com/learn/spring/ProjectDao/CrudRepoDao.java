a399e1346633ae0091e69879e3b3d155029fe424
tharun171 <95117385+tharun171@users.noreply.github.com>
2024-02-24 14:46:07 +0530

    Delete SpringBoot1_Crud_Jpa_PagingRepository directory
    added to wrong repo

--- SpringBoot1_Crud_Jpa_PagingRepository/src/main/java/com/learn/spring/ProjectDao/CrudRepoDao.java
+++
@@ -1,9 +0,0 @@
-package com.learn.spring.ProjectDao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.learn.spring.Entities.EmployeeEntity;
-
-public interface CrudRepoDao extends CrudRepository<EmployeeEntity,Integer>{
-
-}
